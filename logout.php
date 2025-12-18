229bfaac547e431df7a8c9add88f6057b5cffe3e
hlopkov <rorot3100@mail.ru>
2025-12-18 16:33:43 +0300

    ninth commit

--- logout.php
+++ logout.php
@@ -1,5 +1,25 @@
 <?php
 session_start();
-session_destroy();
-header('Location: login.php');
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit();
+}
+
+if (isset($_GET['confirm']) && $_GET['confirm'] == 'yes') {
+    session_destroy();
+    header('Location: login.php');
+    exit();
+}
 ?>
+<!DOCTYPE html>
+<html>
+<head><title>Выход</title></head>
+<body style="text-align: center; padding: 50px;">
+    <p>Вы уверены, что хотите выйти?</p>
+    <p>
+        <a href="logout.php?confirm=yes" style="margin: 0 10px;">Да</a>
+        <a href="javascript:history.back()" style="margin: 0 10px;">Нет</a>
+    </p>
+</body>
+</html>
